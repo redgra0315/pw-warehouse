97f5ff4b5b8f3f0b939026b5322d52c74fd6585c
redgra0315@163.com <redgra0315@163.com>
2019-09-11 10:15:59 +0800

    删除说明

--- python/python Basic/python 基础/Exercises to practice/Judge_how_many.py
+++ python/python Basic/python 基础/Exercises to practice/Judge_how_many.py
@@ -1,10 +1,3 @@
-'''
-@Description: In User Settings Edit
-@Author: your name
-@Date: 2019-09-05 16:16:25
-@LastEditTime: 2019-09-05 16:45:05
-@LastEditors: Please set LastEditors
-'''
 #!/usr/bin/env  python
 
 
