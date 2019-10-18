54919e03b6515a90db94cbcda6bd4e414fbdea16
redgra0315@163.com <redgra0315@163.com>
2019-10-18 18:16:02 +0800

    函数完成，开始练习类

---
+++ python/python Basic/python 基础/modules/mi/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ""
+__author__ = "mi"
+__mtime__ = "2019/10/18"      
+"""
+from  .. import  mi
+
+print(__name__)
+
+__all__ = ('x')
+
+x = 5
